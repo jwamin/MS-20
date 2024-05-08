cc2139291d1d376ea290c6f58c0ee1f82b669a64
Joss Manger <jossferatu@gmail.com>
2024-05-08 08:02:57 -0400

    cli pwm test with keyboard interupt

--- hard-pwm-test.py
+++ hard-pwm-test.py
@@ -1,8 +1,26 @@
 from rpi_hardware_pwm import HardwarePWM
 from time import sleep
+from sys import argv
+
+
 try:
-    pwm = HardwarePWM(pwm_channel=0, hz=60, chip=0)
-    pwm.start(10) # full duty cycle
+
+    default_freq = 60
+    default_duty = 10
+    
+    print(argv)
+    
+    freq:float = float(argv[1]) if len(argv) > 1 else default_freq
+    duty:float = float(argv[2]) if len(argv) > 2 else default_duty
+    
+
+    print(f"freq: {freq}, duty: {duty}")
+    print(f"freq type: {type(freq)}")
+
+    pwm = HardwarePWM(pwm_channel=0, hz=freq, chip=0)
+    pwm.start(duty) # full duty cycle
+
+
 
     #pwm.change_duty_cycle(50)
     #pwm.change_frequency(60)
