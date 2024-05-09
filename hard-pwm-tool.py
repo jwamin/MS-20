15315a5e4147cdb2f939d5eec5cd03a2ad1c81e5
Joss Manger <jossferatu@gmail.com>
2024-05-09 08:57:54 -0400

    adds basic cli config for pwm tool

--- hard-pwm-tool.py
+++ hard-pwm-tool.py
@@ -4,6 +4,11 @@ from rpi_hardware_pwm import HardwarePWM
 from time import sleep
 from sys import argv
 
+PWM_CHANNEL = 0
+PWM_CHIP = 0
+PWM_FREQUENCY = 100 #hz
+PWM_DUTY = 50 #square pulse
+
 def uline_print(str:str, character:chr = "="):
     length = len(str)
     print(f"{str}\n{character*length}")
@@ -13,42 +18,52 @@ try:
     uline_print("Hardware PWM Test Tool")
     print("Pins are configured in /boot/config.txt on your RPi")
 
-    print("channel 0, pin 0, chip 0")
-    print("keyboard interrupt with Ctrl-C to stop Pulse")
-    default_freq = 50 #hz
-    default_duty = 50 #square wave
-    cycle = False
+    print(f"channel {PWM_CHANNEL}, pin 13, chip {PWM_CHIP}")
+    print("Keyboard interrupt with Ctrl-C to stop Pulse")
+    default_freq = PWM_FREQUENCY 
+    default_duty = PWM_DUTY
+    cycle = None
+    rise_and_fall = False
 
     #print(argv)
     
     freq:float = float(argv[1]) if len(argv) > 1 else default_freq
     duty:float = float(argv[2]) if len(argv) > 2 else default_duty
-    cycle: bool = bool(argv[3]) if len(argv) > 3 else cycle
+    cycle:float = float(argv[3]) if len(argv) > 3 else cycle
+    rise_and_fall = bool(argv[4]) if len(argv) > 4 else rise_and_fall
 
-    print(f"freq: {freq}, duty: {duty}")
+    print(f"freq: {freq}, initial duty: {duty} cycle: {cycle} rise_and fall: {rise_and_fall}")
     #print(f"freq type: {type(freq)}")
 
-    pwm = HardwarePWM(pwm_channel=0, hz=freq, chip=0)
+    pwm = HardwarePWM(pwm_channel=PWM_CHANNEL, hz=freq, chip=PWM_CHIP)
     pwm.start(duty) # full duty cycle
 
-    current_duty = 0
-    fps_60:float = 1 / 120
-    total = 100
-    step = total * fps_60
-    rising = True
+
     if cycle:
+        duty_min = 0.1
+        duty_max = 100
+        current_duty = duty_min
+        cycle_freq = cycle 
+        step = cycle_freq / freq
+        rising = rise_and_fall
+        cycle_step = 1 / cycle_freq
+        print(f"cycle freq: {cycle_freq}, step: {step}, cycle_step: {cycle_step}")
         while(True):
             pwm.change_duty_cycle(current_duty)
-            sleep(fps_60)
-            if rising:
-                current_duty = min(current_duty + step,100)
+            sleep(cycle_step)
+            #print(current_duty)
+            if rise_and_fall:
+                if rising:
+                    current_duty = min(current_duty + step,duty_max)
+                else:
+                    current_duty = max(current_duty - step,duty_min)
+                if rise_and_fall:
+                    if current_duty >= duty_max and rising:
+                        rising = False
+                    elif current_duty <= duty_min and not rising:
+                        rising = True
             else:
-                current_duty = max(current_duty - step,0)
-            
-            if current_duty >= 100 and rising:
-                rising = False
-            elif current_duty <= 0 and not rising:
-                rising = True
+                current_duty = min(current_duty + step, duty_max) if current_duty < duty_max else 0 
     else:
         while(True):
             sleep(1)
